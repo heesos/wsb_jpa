c68f15911e37464757b33b132c5d969fe6747bb9
Miłosz <marzec1310@gmail.com>
2025-04-09 21:30:28 +0200

    PatientDAO addVisit method added

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
@@ -1,11 +1,32 @@
 package com.jpacourse.persistance.dao.impl;
 
 import com.jpacourse.persistance.dao.PatientDao;
+import com.jpacourse.persistance.entity.DoctorEntity;
+import com.jpacourse.persistance.entity.MedicalTreatmentEntity;
 import com.jpacourse.persistance.entity.PatientEntity;
+import com.jpacourse.persistance.entity.VisitEntity;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao
-{
+import java.time.LocalDateTime;
+import java.util.List;
 
+@Repository
+public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
+
+    @Override
+    public PatientEntity addVisit(long patientId, long doctorId, LocalDateTime visitTime, List<MedicalTreatmentEntity> medicalTreatmentEntityList, String description) {
+        PatientEntity patient = entityManager.getReference(PatientEntity.class, patientId);
+        DoctorEntity doctor = entityManager.getReference(DoctorEntity.class, doctorId);
+
+        VisitEntity visit = new VisitEntity();
+        visit.setPatient(patient);
+        visit.setDescription(description);
+        visit.setMedicalTreatmentList(medicalTreatmentEntityList);
+        visit.setTime(visitTime);
+        visit.setDoctor(doctor);
+
+        patient.getVisitList().add(visit);
+
+        return entityManager.merge(patient);
+    }
 }
